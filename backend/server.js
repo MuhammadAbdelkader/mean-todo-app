839a6e4fcc39df508111de424614f7a85e81decc
Mohamed <moha7med.abdelkader@gmail.com>
2025-12-06 14:13:22 +0200

    Backend - MEAN Todo App

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,28 @@
-const app = require('./app');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/database');
+const todoRoutes = require('./routes/todo.routes');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/todos', todoRoutes);
+
+// Root route
+app.get('/', (req, res) => {
+  res.json({ message: 'To-Do API is running' });
+});
+
+// Start server
 app.listen(PORT, () => {
-  console.log(`Local server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
